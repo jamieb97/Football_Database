21eab9ff6034fa2c39018e8afec55ad53b0aef2e
Jamie-PC\Jamie <JB1650@student.anglia.ac.uk>
2020-03-23 17:33:28 +0000

    sql query

---
+++ FootballQuery.sql
@@ -0,0 +1,74 @@
+﻿USE master
+GO
+
+DROP DATABASE IF EXISTS FootballDB
+GO
+
+CREATE DATABASE FootballDB
+GO
+
+USE FootballDB
+GO
+
+CREATE TABLE Owners(
+	OwnerID INT NOT NULL PRIMARY KEY IDENTITY,
+	OwnerName VARCHAR(50) NULL,
+	ClubOwned VARCHAR(50) NULL,
+	ClubPercOwned INT NULL,
+	NetWorth INT NULL
+);
+
+CREATE TABLE Agents(
+	AgentID INT NOT NULL PRIMARY KEY IDENTITY,
+	AgentName VARCHAR(50) NULL,
+	AgentFee INT NULL,
+	PlayerPercOwned INT NULL
+);
+
+CREATE TABLE Players(
+	PlayerID INT NOT NULL PRIMARY KEY IDENTITY,
+	PlayerName VARCHAR(50) NULL,
+	PlayerAge INT NULL,
+	OwnerID INT FOREIGN KEY REFERENCES Owners(OwnerID),
+	AgentID INT FOREIGN KEY REFERENCES Agents(AgentID),
+	Salary INT NULL,
+	ContractLength INT NULL
+);
+
+CREATE TABLE Scouts(
+	ScoutID INT NOT NULL PRIMARY KEY IDENTITY,
+	ScoutName VARCHAR(50) NULL,
+	PlayerID INT FOREIGN KEY REFERENCES Players(PlayerID),
+	CountryBased VARCHAR(50)
+);
+
+CREATE TABLE HeadStaffs(
+	HeadStaffID INT NOT NULL PRIMARY KEY IDENTITY,
+	StaffName VARCHAR(50) NULL,
+	OwnerID INT FOREIGN KEY REFERENCES Owners(OwnerID),
+	StaffRole VARCHAR(50) NULL,
+	ScoutID INT FOREIGN KEY REFERENCES Scouts(ScoutID)
+);
+
+INSERT INTO Owners VALUES
+('Sheikh Mansour', 'Manchester City', 100, 17000000000),
+('Stan Kroenke', 'Arsenal', 100, 10000000000)
+GO
+
+INSERT INTO Agents VALUES
+('Mino Raiola', 30000000, 0),
+('Jorge Mendes', 15000000, 5)
+GO
+
+INSERT INTO Players VALUES 
+('Paul Pogba', 27, 0, 0, 250000, 3),
+('Ederson', 26, 0, 1, 160000, 5)
+GO
+
+INSERT INTO Scouts VALUES
+('John Smith', 2, 'Portugal')
+GO
+
+INSERT INTO HeadStaffs VALUES
+('Edu', 2, 'Technical Director', 2)
+GO
